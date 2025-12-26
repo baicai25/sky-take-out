2fdcf03d93b70cf89336c86f992a1d1287965b99
a3535841764 <a3535841764@gmail.com>
2025-12-26 11:37:57 +0800

    redis缓存数据的代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,9 +12,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -27,6 +29,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -38,6 +42,10 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品 :{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清理缓存数据
+        String key = "dish_"+ dishDTO.getCategoryId();
+        clearCache(key);
         return Result.success();
     }
 
@@ -64,6 +72,11 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除 : {}",ids);
         dishService.deleteBantch(ids);
+
+        //将所有的菜品缓存数据都删除,以dish_开头的key
+        clearCache("dish_*");
+
+
         return Result.success();
     }
 
@@ -91,6 +104,11 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品 : {}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //将所有的菜品缓存数据都删除,以dish_开头的key
+        clearCache("dish_*");
+
+
         return Result.success();
     }
 
@@ -113,6 +131,17 @@ public class DishController {
     public Result StartorStop(@PathVariable("status") Integer status, Long id){
         log.info("菜品起售停售: {} , {}",status,id);
         dishService.StartorStop(status,id);
+
+        //将所有的菜品缓存数据都删除,以dish_开头的key
+        clearCache("dish_*");
+
         return Result.success();
     }
+
+
+    private void clearCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
 }
--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -4,7 +4,6 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -31,7 +30,7 @@ public class ShopController {
     @ApiOperation("设置营业状态")
     public Result setStatus(@PathVariable Integer status){
         log.info("设置营业状态: {}",status == 1 ? "营业中" : "打烊了");
-        redisTemplate.opsForValue().set("KEY", status);
+        redisTemplate.opsForValue().set(KEY, status);
         return Result.success();
     }
 
@@ -44,7 +43,7 @@ public class ShopController {
     @ApiOperation("获取当前的营业状态")
     public Result getStatus(){
         //强转的原因是使用Redis的String类型,会将传入的数据转为String,拿出来也是string,这里强转改为Interger,便于接受
-        Integer shopStatus = (Integer) redisTemplate.opsForValue().get("KEY");
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
         log.info("获取当前的营业状态: {}",shopStatus == 1 ? "营业中" : "打烊了");
         return Result.success(shopStatus);
     }
