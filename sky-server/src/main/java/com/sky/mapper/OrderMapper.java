82a7b25bb04e5fb9ad35f03d435de8e4b0259586
a3535841764 <a3535841764@gmail.com>
2026-01-04 18:45:44 +0800

    销量排名统计相关代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import com.sky.result.PageResult;
@@ -100,4 +101,13 @@ public interface OrderMapper {
      * @return
      */
     Integer countByMap(Map map);
+
+
+    /**
+     * 统计指定时间区间的销量排名前十
+     * @param begin
+     * @param end
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
