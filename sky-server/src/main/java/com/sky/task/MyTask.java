c9ff719c6dea170982f796fd55b346d37b049a2c
a3535841764 <a3535841764@gmail.com>
2026-01-05 09:41:32 +0800

    git测试代码5

--- sky-server/src/main/java/com/sky/task/MyTask.java
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -14,6 +14,6 @@ public class MyTask {
 //    @Scheduled(cron = "0/30 * * * * *")
     public void myTask() {
         log.info("定时任务开启,每30s触发,当前时间为; {}",new Date());
-        System.out.println("仅仅是一个测试4");
+        System.out.println("仅仅是一个测试5");
     }
 }
